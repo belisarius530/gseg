4f4216b09af899c68fb34c967ff45762846601ba
belisarius530 <belisarius530@gmail.com>
2014-06-26 19:13:18 -0700

    Added Atmel Studio project file, etc

---
+++ gseg.c
@@ -0,0 +1,17 @@
+/*
+ * gseg.c
+ *
+ * Created: 6/26/2014 7:09:39 PM
+ *  Author: jjfish
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
